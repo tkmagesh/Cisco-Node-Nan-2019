591ec4c860ca436a4acad849b3cb0d3d735d75b9
Magesh <tkmagesh77@gmail.com>
2019-01-24 15:10:18 +0530

    refactoring the parsing logic from serveCalculator to dataParser

--- 08-webAppServer-refactored/dataParser.js
+++ 08-webAppServer-refactored/dataParser.js
@@ -1,9 +1,18 @@
-var url = require('url');
+var url = require('url'),
+	querystring = require('querystring');
 
 module.exports = function(req, res, next){
 	var parsedUrl = url.parse(req.url);
 	Object.assign(req, parsedUrl);
-	next();
+	req.query = querystring.parse(req.query);
+	var rawBodyData = '';
+	req.on('data', function(chunk){
+		rawBodyData += chunk;
+	});
+	req.on('end', function(){
+		req['body'] = querystring.parse(rawBodyData);
+		next();
+	});
 }
 
 
--- 08-webAppServer-refactored/serveCalculator.js
+++ 08-webAppServer-refactored/serveCalculator.js
@@ -1,33 +1,15 @@
-var querystring = require('querystring'),
-	calculator = require('./calculator');
+var calculator = require('./calculator');
 
 module.exports = function(req, res, next){
 	if (req.pathname === '/calculator'){
-		if (req.method === "GET"){
-			var queryData = querystring.parse(req.query),
-				op = queryData.op,
-				n1 = parseInt(queryData.n1),
-				n2 = parseInt(queryData.n2);
-			var result = calculator[op](n1,n2);
-			res.write(result.toString());
-			res.end();
-			next();
-		} else {
-			var rawBodyData = '';
-			req.on('data', function(chunk){
-				rawBodyData += chunk;
-			});
-			req.on('end', function(){
-				var bodyData = querystring.parse(rawBodyData),
-					op = bodyData.op,
-					n1 = parseInt(bodyData.n1),
-					n2 = parseInt(bodyData.n2);
-				var result = calculator[op](n1,n2);
-				res.write(result.toString());
-				res.end();
-				next();
-			});
-		}
+		var data = req.method === 'GET' ? req.query : req.body;
+			op = data.op,
+			n1 = parseInt(data.n1),
+			n2 = parseInt(data.n2);
+		var result = calculator[op](n1,n2);
+		res.write(result.toString());
+		res.end();
+		next();
 	} else {
 		next();
 	}
