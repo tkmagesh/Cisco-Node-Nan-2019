68af99f622770efceace8580a0ef882c4b2ec54c
Magesh <tkmagesh77@gmail.com>
2019-01-24 09:23:09 +0530

    fixing the querystring bug

--- 05-webserver/server.js
+++ 05-webserver/server.js
@@ -1,11 +1,16 @@
 var http = require('http'),
 	fs = require('fs'),
-	path = require('path');
+	path = require('path'),
+	url = require('url');
 
 var server = http.createServer(function(req /* Readable Stream */, res /* Writable Stream*/){
 	console.log('a connection is established - ', req.url );
-	var resourceName = req.url === '/' ? '/index.html' : req.url;
-	var resourceFullName = path.join(__dirname, resourceName);
+	
+	var resource = req.url === '/' ? '/index.html' : req.url,
+	 	parsedUrl = url.parse(resource),
+	 	resourceName = parsedUrl.pathname,
+	 	resourceFullName = path.join(__dirname, resourceName);
+
 	if (!fs.existsSync(resourceFullName)){
 		res.statusCode = 404;
 		res.end();
