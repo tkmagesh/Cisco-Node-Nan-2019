e407c2e398870acebfd31e4bfc29d0fd688c384c
Magesh <tkmagesh77@gmail.com>
2019-01-24 11:15:30 +0530

    handling post requests

--- 06-appServer/server.js
+++ 06-appServer/server.js
@@ -6,24 +6,41 @@ var http = require('http'),
 	calculator = require('./calculator');
 
 var server = http.createServer(function(req /* Readable Stream */, res /* Writable Stream*/){
-	console.log('a connection is established - ', req.url );
 	
 	var resource = req.url,
 	 	parsedUrl = url.parse(resource),
-	 	resourceName = parsedUrl.pathname,
-	 	queryData = querystring.parse(parsedUrl.query);
+	 	resourceName = parsedUrl.pathname;
 	 	
+	console.log(req.method + '\t' + resourceName);
+	
 	if (resourceName !== '/calculator'){
 		res.statusCode = 404;
 		res.end();
 		return;
 	}
-	var op = queryData.op,
-		n1 = parseInt(queryData.n1),
-		n2 = parseInt(queryData.n2);
-	var result = calculator[op](n1,n2);
-	res.write(result.toString());
-	res.end();
+	if (req.method === "GET"){
+		var queryData = querystring.parse(parsedUrl.query),
+			op = queryData.op,
+			n1 = parseInt(queryData.n1),
+			n2 = parseInt(queryData.n2);
+		var result = calculator[op](n1,n2);
+		res.write(result.toString());
+		res.end();
+	} else {
+		var rawBodyData = '';
+		req.on('data', function(chunk){
+			rawBodyData += chunk;
+		});
+		req.on('end', function(){
+			var bodyData = querystring.parse(rawBodyData),
+				op = bodyData.op,
+				n1 = parseInt(bodyData.n1),
+				n2 = parseInt(bodyData.n2);
+			var result = calculator[op](n1,n2);
+			res.write(result.toString());
+			res.end();
+		});
+	}
 });
 
 server.listen(8085);
